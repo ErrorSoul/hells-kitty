e458ff5370fed5e7daff3ec3cbeff232c86b8ba7
Abu Sultanov <neurofag@gmail.com>
2016-05-12 02:59:17 +0300

    Complete sizes without :destroy

--- app/controllers/admin/sizes_controller.rb
+++ app/controllers/admin/sizes_controller.rb
@@ -6,20 +6,39 @@ class Admin::SizesController < Admin::BaseController
 
   def new
     @size = Size.new
+    flash[:success] = t 'flash.size.success.created'
   end
 
   def show
+    @size = Size.find params[:id]
+  end
+
+  def edit
+    @size = Size.find params[:id]
+  end
+
+  def update
+    @size = Size.find params[:id]
+
+    if @size.update_attributes size_params
+      redirect_to admin_size_path(@size)
+      flash[:success] = t 'flash.size.success.updated'
+    else
+      flash[:error] = @size.errors.full_messages.join('<br/>').html_safe
+      render :edit
+    end
   end
 
   def create
     @size = Size.new size_params
 
     if @size.save
-      redirect_to admin_sizes_path
+      redirect_to admin_size_path(@size)
       flash[:success] = t 'flash.size.success.created'
     else
-      render :new
       flash[:error] = @size.errors.full_messages.join('<br/>').html_safe
+      render :new
+
     end
   end
 
