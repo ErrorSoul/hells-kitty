d18129bed8eb94b70371133adfa62c28d538cbaf
Abu Sultanov <neurofag@gmail.com>
2016-05-14 05:19:55 +0300

    Fix

--- app/controllers/admin/sizes_controller.rb
+++ app/controllers/admin/sizes_controller.rb
@@ -1,25 +1,20 @@
 class Admin::SizesController < Admin::BaseController
 
+  before_action :find_size, only: [:show, :edit, :update, :destroy]
+
   def index
     @sizes = Size.all
   end
 
   def new
     @size = Size.new
-    flash[:success] = t 'flash.size.success.created'
   end
 
-  def show
-    @size = Size.find params[:id]
-  end
+  def show; end
 
-  def edit
-    @size = Size.find params[:id]
-  end
+  def edit; end
 
   def update
-    @size = Size.find params[:id]
-
     if @size.update_attributes size_params
       redirect_to admin_size_path(@size)
       flash[:success] = t 'flash.size.success.updated'
@@ -42,8 +37,21 @@ class Admin::SizesController < Admin::BaseController
     end
   end
 
+  def destroy
+    if @size.destroy
+      redirect_to admin_sizes_path
+      flash[:success] =  t 'flash.size.success.deleted'
+    else
+      flash[:destroy] = t 'flash.size.fail.deleted'
+    end
+  end
+
   def size_params
     params[:size].permit!
+  end
+
+  def find_size
+    @size = Size.find params[:id]
   end
 
 end
