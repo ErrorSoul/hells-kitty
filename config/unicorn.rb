8c55d760bce46fcb53c7a3d271a897089af4d0f5
Muslim Sultanov <muslim.sultanov@uchi.ru>
2016-09-21 14:50:57 +0300

    new unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,7 @@ working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
-
+old_pid  "#{pid}.oldbin"
 listen "#{root}/tmp/sockets/unicorn.sock", backlog: 64
 
 worker_processes 2
@@ -12,30 +12,18 @@ timeout 30
 preload_app true
 
 
-# Force the bundler gemfile environment variable to
-# reference the capistrano "current" symlink
-before_exec do |_|
-  ENV['BUNDLE_GEMFILE'] = File.join(root, 'Gemfile')
+GC.copy_on_write_friendly = true if GC.respond_to?(:copy_on_write_friendly=) # Решительно не уверен, что значит эта строка, но я решил ее оставить.
+
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = "#{rails_root}/Gemfile"
 end
 
-
 before_fork do |server, worker|
+  # Перед тем, как создать первый рабочий процесс, мастер отсоединяется от базы.
+  defined?(ActiveRecord::Base) and
+  ActiveRecord::Base.connection.disconnect!
 
-  # Clear DB Connections. See `after_fork`!
-  ActiveRecord::Base.connection_handler.clear_all_connections!
-
-  ##
-  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
-  # immediately start loading up a new version of itself (loaded with a new
-  # version of our app). When this new Unicorn is completely loaded
-  # it will begin spawning workers. The first worker spawned will check to
-  # see if an .oldbin pidfile exists. If so, this means we've just booted up
-  # a new Unicorn and need to tell the old one that it can now die. To do so
-  # we send it a QUIT.
-  #
-  # Using this method we get 0 downtime deploys.
-
-  old_pid = "#{pid}.oldbin"
+  # Ниже идет магия, связанная с 0 downtime deploy.
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
@@ -46,5 +34,7 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
-  ActiveRecord::Base.connection_handler.verify_active_connections!
+  # После того как рабочий процесс создан, он устанавливает соединение с базой.
+  defined?(ActiveRecord::Base) and
+  ActiveRecord::Base.establish_connection
 end
