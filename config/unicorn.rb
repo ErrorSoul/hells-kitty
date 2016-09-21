0b0869150b533f9f9407f5b680e8cab1de80cb0d
Muslim Sultanov <muslim.sultanov@uchi.ru>
2016-09-21 17:10:48 +0300

    fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,22 +10,3 @@ listen "#{root}/tmp/sockets/unicorn.sock", backlog: 64
 worker_processes 2
 timeout 30
 preload_app true
-
-
-GC.copy_on_write_friendly = true if GC.respond_to?(:copy_on_write_friendly=) # Решительно не уверен, что значит эта строка, но я решил ее оставить.
-
-before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = "#{rails_root}/Gemfile"
-end
-
-before_fork do |server, worker|
-  # Перед тем, как создать первый рабочий процесс, мастер отсоединяется от базы.
-  defined?(ActiveRecord::Base) and
-  ActiveRecord::Base.connection.disconnect!
-end
-
-after_fork do |server, worker|
-  # После того как рабочий процесс создан, он устанавливает соединение с базой.
-  defined?(ActiveRecord::Base) and
-  ActiveRecord::Base.establish_connection
-end
