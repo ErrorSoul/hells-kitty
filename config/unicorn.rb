89119c181d3a7ee97fbe9ea36bf81484def3ea8e
Muslim Sultanov <muslim.sultanov@uchi.ru>
2016-09-21 17:38:09 +0300

    fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,3 +10,20 @@ listen "#{root}/tmp/sockets/unicorn.sock", backlog: 64
 worker_processes 2
 timeout 30
 preload_app true
+
+
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = "#{rails_root}/Gemfile"
+end
+
+before_fork do |server, worker|
+  # Перед тем, как создать первый рабочий процесс, мастер отсоединяется от базы.
+  defined?(ActiveRecord::Base) and
+  ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  # После того как рабочий процесс создан, он устанавливает соединение с базой.
+  defined?(ActiveRecord::Base) and
+  ActiveRecord::Base.establish_connection
+end
