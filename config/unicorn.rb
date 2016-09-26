4cab2df19b033731dba71da6321533e2f677b893
Muslim Sultanov <muslim.sultanov@uchi.ru>
2016-09-26 19:45:02 +0300

    wip

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,8 +8,9 @@ listen "#{root}/tmp/sockets/unicorn.sock", backlog: 64
 
 worker_processes 2
 timeout 30
-preload_app true
-
+#preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
 
 before_exec do |server|
   ENV["BUNDLE_GEMFILE"] = "#{root}/Gemfile"
