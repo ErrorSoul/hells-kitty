75a6ff654f09fab9d604bf4df879cad32a2b298d
Muslim Sultanov <muslim.sultanov@uchi.ru>
2016-10-27 00:01:55 +0300

    WIP: среда, 26 октября 2016 г. 23:12:50 (MSK)

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -1,9 +1,11 @@
 class ExchangesController < ApplicationController
+
+  skip_before_action :verify_authenticity_token, only: :create
   def index
     puts params.inspect
     if params[:type] == 'catalog'
       if params[:mode] == 'checkauth'
-        puts request.inspect
+        #puts request.inspect
         render text: "success\n"
       elsif params[:mode] == 'init'
         render text: "zip=yes\nfile_limit=2000\n"
