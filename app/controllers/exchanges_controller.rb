834424c79b0cf57b3374393018bdfabcc57c903a
Muslim Sultanov <muslim.sultanov@uchi.ru>
2016-10-26 23:35:59 +0300

    WIP: среда, 26 октября 2016 г. 23:12:50 (MSK)

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -2,7 +2,10 @@ class ExchangesController < ApplicationController
   def index
     puts params.inspect
     if params[:type] == 'catalog'
-      render text: "success\n"
+      if params[:mode] == 'checkauth'
+        render text: "success\n"
+      elsif params[:mode] == 'init'
+        render texxt: "zip=yes\n"
     end
   end
 end
